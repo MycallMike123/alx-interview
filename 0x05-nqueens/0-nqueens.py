3be6f27cc5b843f2945f3fe406dd92437aebd5c5
Michael Mwangi <michaelmichire2016@gmail.com>
2024-08-01 05:16:29 -0700

    0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-Solution to the nqueens problem
+Implementation of the N-Queens problem
 """
 
 import sys
@@ -9,28 +9,29 @@ import sys
 
 def backtrack(r, n, cols, pos, neg, board):
     """
-    Backtrack function to solve nqueens problem
+    Recursive backtracking function to solve N-Queens
     Args:
-        r (int): current row
-        n (int): number of queens
-        cols (set): set of columns occupied by queens
-        pos (set): set of positive diagonals occupied by queens
-        neg (set): set of negative diagonals occupied by queens
-        board (list): chess board
+        r (int): the current row being processed
+        n (int): total number of queens
+        cols (set): columns occupied by queens
+        pos (set): positive diagonals occupied by queens
+        neg (set): negative diagonals occupied by queens
+        board (list): representation of the chessboard
+    Returns:
+        None
     """
     if r == n:
-        res = []
+        solution = []
         for i in range(len(board)):
             for k in range(len(board[i])):
                 if board[i][k] == 1:
-                    res.append([i, k])
-        # Print the coordinates of the queens
-        print(res)
+                    solution.append([i, k])
+        # Output the queen positions
+        print(solution)
         return
 
     for c in range(n):
-        # Check if the column or positive diagonal or
-        # negative diagonal is occupied
+        # Skip if the column or either diagonal is under attack
         if c in cols or (r + c) in pos or (r - c) in neg:
             continue
 
@@ -40,10 +41,10 @@ def backtrack(r, n, cols, pos, neg, board):
         neg.add(r - c)
         board[r][c] = 1
 
-        # Recursively call the backtrack function for the next row
+        # Recurse to the next row
         backtrack(r+1, n, cols, pos, neg, board)
 
-        # Remove the queen from the board
+        # Remove the queen to backtrack
         cols.remove(c)
         pos.remove(r + c)
         neg.remove(r - c)
@@ -52,17 +53,16 @@ def backtrack(r, n, cols, pos, neg, board):
 
 def nqueens(n):
     """
-    Solution to nqueens problem
+    Solve the N-Queens problem
     Args:
         n (int): number of queens. Must be >= 4
-    Return:
-        List of lists representing coordinates of each
-        queen for all possible solutions
+    Returns:
+        None
     """
     cols = set()
     pos_diag = set()
     neg_diag = set()
-    board = [[0] * n for i in range(n)]
+    board = [[0] * n for _ in range(n)]
 
     backtrack(0, n, cols, pos_diag, neg_diag, board)
 
