c07061d2f70d679c8807fc9755b86daae20473a8
Michael Mwangi <michaelmichire2016@gmail.com>
2024-08-06 08:09:34 -0700

    A script that prints all characters of a Star Wars movie

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,63 +2,6 @@
 
 const request = require('request');
 
-if (process.argv.length < 3) {
-  console.log('Usage: ./0-starwars_characters.js <Movie.id>');
-  process.exit(1);
-}
-
-const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-// Function to retrieve data from a specified URL
-const fetchData = (url) => {
-  return new Promise((resolve, reject) => {
-    request(url, (error, response, body) => {
-      if (error) {
-        reject(error); // Handle request errors
-      } else if (response.statusCode !== 200) {
-        reject(
-          new Error(`Request failed, status code: ${response.statusCode}`)
-        ); // Handle non-success HTTP status codes
-      } else {
-        resolve(JSON.parse(body)); // Parse and return the response body as JSON
-      }
-    });
-  });
-};
-
-// Retrieve movie details and then get character information
-fetchData(apiUrl)
-  .then((movieData) => {
-    const characterPromises = movieData.characters.map(
-      (characterUrl) => fetchData(characterUrl) // Create promises for each character URL
-    );
-    return Promise.all(characterPromises); // Wait for all character data to be retrieved
-  })
-  .then((characters) => {
-    characters.forEach((character) => {
-      console.log(character.name); // Output each character's name
-    });
-  })
-  .catch((error) => {
-    console.log('Error:', error.message); // Handle errors during the process
-  });
-The comments have been rephrased while retaining the original meaning.
-
-
-
-
-
-lets put the comments above the  code instead of being in line
-ChatGPT
-Here's the code with comments placed above the corresponding sections
-
-javascript
-Copy code
-#!/usr/bin/node
-
-const request = require('request');
-
 // Ensure the correct number of arguments is provided
 if (process.argv.length < 3) {
   console.log('Usage: ./0-starwars_characters.js <Movie.id>');
